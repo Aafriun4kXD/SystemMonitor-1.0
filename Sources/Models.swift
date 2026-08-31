aea589b292d3a781dda401bd0566b1ef4d2c6a6b
Aafriun4kXD <quien228@gmail.com>
2026-08-31 19:48:40 +0300

    Add SystemStats struct for system resource tracking

---
+++ Sources/Models.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+struct SystemStats {
+    var cpuUsage: Double = 0
+    var ramUsed: Double = 0
+    var ramTotal: Double = 0
+    var batteryLevel: Int = 0
+    var batteryState: String = "Unknown"
+}
