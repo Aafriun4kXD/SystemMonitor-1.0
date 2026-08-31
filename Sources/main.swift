ad07c346069ef9d30eeb0443f5b1e1716432c988
Aafriun4kXD <quien228@gmail.com>
2026-08-31 19:38:11 +0300

    Initialize SystemMonitorApp with SwiftUI structure

---
+++ Sources/main.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+import UIKit
+import Foundation
+
+@main
+struct SystemMonitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
